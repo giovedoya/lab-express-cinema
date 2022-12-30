a73f4e7fc17fdf593afe771991ca3a325908b459
Giovanni Bedoya <giovedoya@gmail.com>
2022-12-30 13:03:23 +0100

    'Movies page without css'

--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('../models/Movie.model');
 
 /* GET movies page */
 /* ROUTE movies */
-router.get('/', (req, res, next) => res.render('movies'));
+router.get('/', async function (req, res, next) {
+    try {
+        const movies = await Movies.find({})
+        res.render('movies', { movies });
+    } catch (error) {
+      next(error)  
+    }   
+})
 
 module.exports = router;
